f31454b9a7da4445869fdd03eff1e8c169ab8456
geovanyGonz <tigregonzo34@gmail.com>
2023-10-04 22:02:23 -0500

    Cree las carpetas de Css, j.s con sus archivos carrito.js y main.js con los archivos html

---
+++ js/carrito.js
@@ -0,0 +1,14 @@
+const productosEnCarrito = JSON.parse(localStorage.getItem("productos-en-carrito"));
+
+const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
+const contenedorCarritoProductos = document.querySelector("#carrito-productos");
+const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
+const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
+
+if (productosEnCarrito) {
+    contenedorCarritoVacio.classList.add("disabled");
+    contenedorCarritoProductos.classList.remove("disabled");
+    contenedorCarritoAcciones.classList.remove("disabled");
+    contenedorCarritoComprado.classList.add("disabled");
+
+} 
