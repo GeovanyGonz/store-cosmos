0c3b95f28392a572dc132bc7889926798003f4e2
geovanyGonz <tigregonzo34@gmail.com>
2023-11-09 22:30:45 -0500

    Agregue codigo main.css, index.html, agregue el archivo archivo menu.js, agregue la libreria toastify para alerts

--- js/main.js
+++ js/main.js
@@ -79,6 +79,20 @@ if (productosEnCarritoLS) {
 }
 
 function agregarAlCarrito(e) {
+    Toastify({
+        text: "This is a toast",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "left", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
     const idBoton = e.currentTarget.id;
     const productoAgregado = productos.find(producto => producto.id === idBoton);
 
